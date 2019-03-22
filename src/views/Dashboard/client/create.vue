ac0cb248b07141f7fa83d621e1891ae87d0bffa4
Eduardo Reis Nobre <eduardo.reisnobre@gmail.com>
2019-03-22 14:16:28 -0300

    service semantic html5

--- src/views/Dashboard/client/create.vue
+++ src/views/Dashboard/client/create.vue
@@ -1,72 +1,75 @@
 <template>
-  <main class="content" id="createClient">
-    <section class="client">
-      <h3>Cliente</h3>
-      <article>
-        <b-field label="Nome">
-          <b-input v-model="client.name" v-validate="'alpha'" placeholder="Nome" name="name" required></b-input>
+  <main id="createClient">
+    <h3>Cliente</h3>
+    <div class="content">
+      <section class="client">
+        <article>
+          <b-field label="Nome">
+            <b-input v-model="client.name" v-validate="'alpha'" placeholder="Nome" name="name" required></b-input>
+          </b-field>
+          <span>{{ errors.first('name') }}</span>
+          <b-field label="Telefone">
+            <b-input v-model="client.phone" v-mask="'(##) # ####-####'" placeholder="Telefone" required></b-input>
+          </b-field>
+          <b-field label="Data de Nascimento">
+            <b-input v-model="user.birthdate" v-validate="rules.birthdate" v-mask="'##/##/####'" placeholder="10/10/1994" name="birthdate" required></b-input>
+          </b-field>
+          <span>{{ errors.first('birthdate') }}</span>
+        </article>
+        <article class="info-second">
+          <b-field label="Email">
+            <b-input v-model="user.email" type="email"  v-validate="rules.email" placeholder="exemplo@exemplo.com" name="Email" required></b-input>
+          </b-field>
+          <div class="block">
+            <b-radio v-model="user.type_id" native-value="1">
+              Juridico
+            </b-radio>
+            <b-radio v-model="user.type_id" native-value="2">
+              Fisico
+            </b-radio>
+          </div>
+        </article>
+        <b-field label="CPF/CNPJ">
+          <b-input v-if="user.type_id" v-model="user.document" v-validate="rules.document" v-mask="user.type_id === '2' ? '###.###.###-##' : '##.###.###/####-##'" placeholder="Documentos" name="document" required></b-input>
+          <b-input v-else v-model="user.document" v-validate="rules.document" v-mask="user.type_id === '2' ? '###.###.###-##' : '##.###.###/####-##'" placeholder="Documentos" name="document" disabled></b-input>
         </b-field>
-        <span>{{ errors.first('name') }}</span>
-        <b-field label="Telefone">
-          <b-input v-model="client.phone" v-mask="'(##) # ####-####'" placeholder="Telefone" required></b-input>
+        <address class="address">
+          <h3>Endereço</h3>
+          <article class="info-three">
+            <b-field label="Rua">
+              <b-input v-model="address.address" placeholder="Rua" name="address" required></b-input>
+            </b-field>
+            <b-field label="Estado">
+              <b-input v-model="address.state"  v-validate="'alpha'" laceholder="ES" name="state" required></b-input>
+            </b-field>
+          </article>
+          <article class="info-fourth">
+            <b-field label="CEP">
+              <b-input v-model="address.postal_code" v-mask="'##.###-###'" v-validate="rules.postal_code" name="postal_code" placeholder="CEP" required></b-input>
+            </b-field>
+            <b-field label="Bairro">
+              <b-input v-model="address.neighborhood" v-validate="'alpha'" placeholder="Bairro"  name="neighborhood" required></b-input>
+            </b-field>
+            <b-field label="Cidade">
+              <b-input v-model="address.city" v-validate="'alpha'" placeholder="Cidade" required name="city"></b-input>
+            </b-field>
+          </article>
+        </address>
+      </section>
+      <aside class="others">
+        <h3>Outros</h3>
+        <b-field label="Observações">
+          <vue-editor :editorToolbar="customToolbar" v-model="client.observations" placeholder="Analise de dados"></vue-editor>
         </b-field>
-        <b-field label="Data de Nascimento">
-          <b-input v-model="user.birthdate" v-validate="rules.birthdate" v-mask="'##/##/####'" placeholder="10/10/1994" name="birthdate" required></b-input>
+        <b-field label="Atividade">
+          <b-input v-model="client.activity" placeholder="Produçaõ de PANIC" required></b-input>
         </b-field>
-        <span>{{ errors.first('birthdate') }}</span>
-      </article>
-      <article class="info-second">
-        <b-field label="Email">
-          <b-input v-model="user.email" type="email"  v-validate="rules.email" placeholder="exemplo@exemplo.com" name="Email" required></b-input>
-        </b-field>
-        <div class="block">
-          <b-radio v-model="user.type_id" native-value="1">
-            Juridico
-          </b-radio>
-          <b-radio v-model="user.type_id" native-value="2">
-            Fisico
-          </b-radio>
+        <div class="buttonClass">
+          <button @click="createClient">Cadastrar</button>
         </div>
-      </article>
-      <b-field label="CPF/CNPJ">
-        <b-input v-if="user.type_id" v-model="user.document" v-validate="rules.document" v-mask="user.type_id === '2' ? '###.###.###-##' : '##.###.###/####-##'" placeholder="Documentos" name="document" required></b-input>
-        <b-input v-else v-model="user.document" v-validate="rules.document" v-mask="user.type_id === '2' ? '###.###.###-##' : '##.###.###/####-##'" placeholder="Documentos" name="document" disabled></b-input>
-      </b-field>
-      <address class="address">
-        <h3>Endereço</h3>
-        <article class="info-three">
-          <b-field label="Rua">
-            <b-input v-model="address.address" placeholder="Rua" name="address" required></b-input>
-          </b-field>
-          <b-field label="Estado">
-            <b-input v-model="address.state"  v-validate="'alpha'" laceholder="ES" name="state" required></b-input>
-          </b-field>
-        </article>
-        <article class="info-fourth">
-          <b-field label="CEP">
-            <b-input v-model="address.postal_code" v-mask="'##.###-###'" v-validate="rules.postal_code" name="postal_code" placeholder="CEP" required></b-input>
-          </b-field>
-          <b-field label="Bairro">
-            <b-input v-model="address.neighborhood" v-validate="'alpha'" placeholder="Bairro"  name="neighborhood" required></b-input>
-          </b-field>
-          <b-field label="Cidade">
-            <b-input v-model="address.city" v-validate="'alpha'" placeholder="Cidade" required name="city"></b-input>
-          </b-field>
-        </article>
-      </address>
-    </section>
-    <aside class="others">
-      <h3>Outros</h3>
-      <b-field label="Observações">
-        <vue-editor :editorToolbar="customToolbar" v-model="client.observations" placeholder="Analise de dados"></vue-editor>
-      </b-field>
-      <b-field label="Atividade">
-        <b-input v-model="client.activity" placeholder="Produçaõ de PANIC" required></b-input>
-      </b-field>
-      <div class="buttonClass">
-        <button @click="createClient">Cadastrar</button>
-      </div>
-    </aside>
+      </aside>
+
+    </div>
   </main>
 </template>
 <script>
--- src/views/Dashboard/services/create.vue
+++ src/views/Dashboard/services/create.vue
@@ -1,82 +1,88 @@
 <template>
-  <div id="createService">
-    <div id="createService">
-      <div class="service-informations" v-if="selected != undefined">
-        <div class="description-service">
-          <b-field label="Título">
-            <textarea v-model="service.name" name=""  cols="50" rows="2"></textarea>
+  <main id="createService">
+    <h3>Serviço</h3>
+    <div class="content">
+      <section class="service">
+        <b-field label="Título">
+          <textarea v-model="service.name" name=""  cols="50" rows="2"></textarea>
+        </b-field>
+        <b-field label="Cliente">
+          <b-input v-model="selected.client.name" placeholder="Cliente" disabled></b-input>
+        </b-field>
+        <article class="info-two">
+          <b-field label="Previsão">
+            <b-input v-model="service.forecast" v-validate="'regex: rules.date_before.regex, before: beforeTarget'" v-mask="'##/##/####'" placeholder="data" name="date" required></b-input>
           </b-field>
-          <div class="info-two">
-            <b-field label="Previsão">
-              <b-input v-model="service.forecast" v-validate="'regex: rules.date_before.regex, before: beforeTarget'" v-mask="'##/##/####'" placeholder="data" name="date" required></b-input>
-            </b-field>
-            <b-field label="Prazo">
-              <b-input v-model="service.due_date" ref="beforeTarget" v-validate="rules.date_before" v-mask="'##/##/####'" placeholder="data" name="beforeTarget" required></b-input>
-            </b-field>
-          </div>
+          <b-field label="Prazo">
+            <b-input v-model="service.due_date" ref="beforeTarget" v-validate="rules.date_before" v-mask="'##/##/####'" placeholder="data" name="beforeTarget" required></b-input>
+          </b-field>
           <span>{{ errors.first('date') }}</span>
           <span>{{ errors.first('beforeTarget') }}</span>
-          <div class="info-three">
-            <b-field label="Cliente">
-              <b-input v-model="selected.client.name" placeholder="Cliente" disabled></b-input>
-            </b-field>
-            <b-field label="Sigilo">
-              <div class="block">
-                <b-radio v-model="radio" native-value="Nenhum">
-                  Nenhum
-                </b-radio>
-                <b-radio v-model="radio" native-value="Parcial">
-                  Parcial
-                </b-radio>
-                <b-radio v-model="radio" native-value="Total">
-                  Total
-                </b-radio>
-              </div>
-            </b-field>
-          </div>
-          <div class="info-four">
-            <b-field label="Margem">
-              <b-input v-model="service.profit" placeholder="50%" v-validate="rules.number" name="margem" required></b-input>
-            </b-field>
-            <b-field label="Valor">
-              <b-input v-model.lazy="service.value" v-money="money" placeholder="825" name="valor" required></b-input>
-            </b-field>
-            <b-field label="Recebido">
-              <b-input placeholder="825" name="recebido" required></b-input>
-            </b-field>
-          </div>
-          <span>{{ errors.first('margem') }}</span>
-          <span>{{ errors.first('valor') }}</span>
-          <span>{{ errors.first('recebido') }}</span>
-          <b-field label="Situação">
-            <b-select placeholder="Select a name">
-              <option value="">Selecione</option>
-              <option value="1">ORCA - Orçamento (em aberto)</option>
-            </b-select>
+        </article>
+        <article class="info-three">
+          <b-field label="Sigilo">
+            <div class="block">
+              <b-radio v-model="radio" native-value="Nenhum">
+                Nenhum
+              </b-radio>
+              <b-radio v-model="radio" native-value="Parcial">
+                Parcial
+              </b-radio>
+              <b-radio v-model="radio" native-value="Total">
+                Total
+              </b-radio>
+            </div>
           </b-field>
-        </div>
+        </article>
+        <article class="info-four">
+          <b-field label="Margem">
+            <b-input v-model="service.profit" placeholder="50%" v-validate="rules.number" name="margem" required></b-input>
+          </b-field>
+          <b-field label="Valor">
+            <b-input v-model.lazy="service.value" v-money="money" placeholder="825" name="valor" required></b-input>
+          </b-field>
+          <b-field label="Recebido">
+            <b-input placeholder="825" name="recebido" required></b-input>
+          </b-field>
+        </article>
+        <span>{{ errors.first('margem') }}</span>
+        <span>{{ errors.first('valor') }}</span>
+        <span>{{ errors.first('recebido') }}</span>
+        <b-field label="Situação">
+          <b-select placeholder="Select a name">
+            <option value="">Selecione</option>
+            <option value="1">ORCA - Orçamento (em aberto)</option>
+          </b-select>
+        </b-field>
+      </section>
+      <section>
         <div class="description">
           <b-field label="Observações">
-            <textarea v-model="service.description" name="" id="" cols="35" rows="15"></textarea>
+            <vue-editor :editorToolbar="customToolbar" v-model="service.description" placeholder="Analise de dados"></vue-editor>
           </b-field>
-          <div class="buttonsCreate">
+          <div class="buttonClass">
             <button class="is-primary" @click="create">Cadastrar</button>
           </div>
         </div>
-      </div>
+      </section>
     </div>
-  </div>
+  </main>
 </template>
 <script>
 import { mapGetters } from 'vuex'
 import { header } from '../../../config/index.js'
 import userCreate from '../../../mixins/userCreate'
+import { VueEditor } from 'vue2-editor'
 export default {
   name: 'createService',
   props: ['open', 'selected.client.name', 'selected.client.id'],
   mixins: [userCreate],
   data () {
     return {
+      customToolbar: [
+        ['bold', 'italic', 'underline'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }]
+      ],
       radio: '',
       beforeTarget: '',
       service: {
@@ -125,6 +131,9 @@ export default {
         // this.$emit('update:open', false)
       })
     }
+  },
+  components: {
+    VueEditor
   }
 }
 </script>
