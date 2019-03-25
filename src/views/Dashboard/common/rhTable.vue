0b1c61bdca63386723090326f504386ea398306d
Eduardo Reis Nobre <eduardo.reisnobre@gmail.com>
2019-03-25 20:42:38 -0300

    added create layout config

--- src/views/Dashboard/common/rhTable.vue
+++ src/views/Dashboard/common/rhTable.vue
@@ -1,13 +1,17 @@
 <template>
 	<section class="__secundary">
-		<div class="tableContainer">
+		<div :class="[{create: create}, 'tableContainer']">
 			<header>
 				<h4>
 					<slot name="title"></slot>
-					<div id="edit" @click="isEditActive = true">
-						<b-icon icon="search"></b-icon>
-					</div>
 				</h4>
+				<button v-if="create" class="buttons is-primary" @click="isModalActive = true">Cadastrar novo Rh</button>
+				<b-field>
+					<b-input placeholder="Procurar"></b-input>
+				</b-field>
+				<div id="edit" @click="isEditActive = true">
+					<b-icon icon="cog"></b-icon>
+				</div>
 			</header>
 			<b-table :data="rhs" :paginated="true" :selected.sync="selected" :per-page="5" @update:selected="$emit('update', $event)" focusable style="padding-top: 1rem">
 				<template slot-scope="props">
@@ -22,17 +26,25 @@
 					</b-table-column>
 				</template>
 			</b-table>
+			<b-modal :active.sync="isModalActive">
+				<component :is="parseModal()" @rhCreated="rhCreated = true" @creationFailed="rhCreated = false"></component>
+			</b-modal>
 		</div>
 	</section>
 </template>
 <script>
 import moment from 'moment'
+import createRh from '../rh/create.vue'
+import success from './create-messages/success'
+import error from './create-messages/error'
 export default {
 	name: 'rhTable',
-	props: ['rhs'],
+	props: ['rhs', 'create'],
 	data () {
 		return {
-			rhSelected: undefined
+			isModalActive: false,
+			rhSelected: undefined,
+			rhCreated: undefined
 		}
 	},
 	computed: {
@@ -48,7 +60,20 @@ export default {
 	methods: {
     parseDate (date) {
       return moment().format('DD/MM/YYYY')
+    },
+    parseModal () {
+      if (this.rhCreated === undefined) {
+        return 'createRh'
+      } else if (this.rhCreated === true) {
+        return 'success'
+      }
+      return 'error'
     }
+	},
+	components: {
+		createRh,
+		success,
+		error
 	}
 }
 </script>
