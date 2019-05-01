803e989eda8ef3efeb3b1a1c7c7fda3b306c8b51
Eduardo Reis Nobre <eduardo.reisnobre@gmail.com>
2019-05-01 19:56:57 -0300

    init

--- src/stores/serviceStore.js
+++ src/stores/serviceStore.js
@@ -1,16 +1,37 @@
 import { header } from '../config/index.js'
 
 const state = {
-  services: []
+  services: [],
+  serviceSelected: undefined
 }
 
 const getters = {
-  services: () => state.services
+  services: () => state.services,
+  serviceSelected: () => state.serviceSelected
 }
 
 const mutations = {
   SET_SERVICES (state, services) {
     state.services = services
+  },
+  SET_SERVICE_SELECTED (state, service) {
+    state.serviceSelected = JSON.parse(JSON.stringify(service))
+  },
+  UPDATE_SERVICE_SELECTED (state, payload) {
+    const label = payload[0]
+    const target = payload[1]
+    const value = payload[2]
+    switch (target) {
+      case 'user':
+        state.clientSelected.user[label] = value
+        break
+      case 'address':
+        state.clientSelected.user.address[label] = value
+        break
+      case '':
+        state.clientSelected[label] = value
+        break
+    }
   },
   UPDATE_SERVICE (state, payload) {
     const service = payload[0]
